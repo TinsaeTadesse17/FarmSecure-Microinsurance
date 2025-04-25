9ad32db1b476eebfee4e2a894a1b6e6ad9f8919d
Tinsae Tadesse <tinsaetadesse769@gmail.com>
2025-04-25 15:05:44 +0300

    add entrypoint

---
+++ services/claim_management/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# entrypoint.sh
+
+# Run initial migrations (if no migration exists, alembic will create one based on your autogenerate settings)
+alembic upgrade head
+
+# Start the FastAPI application
+exec uvicorn src.main:app --host 0.0.0.0 --port 8001
