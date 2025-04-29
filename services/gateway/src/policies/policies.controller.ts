bb0284010808c512a7598b075722f9f1f0564541
Samrwitt <samrawitkta@gmail.com>
2025-04-29 20:11:51 +0300

    little tweaks

--- services/gateway/src/policies/policies.controller.ts
+++ services/gateway/src/policies/policies.controller.ts
@@ -1,41 +1,41 @@
 import { Controller, Post, Get, Param, Body } from '@nestjs/common';
 import { PoliciesService } from './policies.service';
 
-@Controller('policies')
+@Controller('api')
 export class PoliciesController {
   constructor(private readonly policyService: PoliciesService) {}
 
-  @Post()
+  @Post('/policy')
   async createPolicy(@Body() payload: any) {
     return this.policyService.createPolicy(payload);
   }
 
-  @Post(':policyId/approve')
+  @Post('/policy:policyId/approve')
   async approvePolicy(@Param('policyId') policyId: number) {
     return this.policyService.approvePolicy(policyId);
   }
 
-  @Post(':policyId/reject')
+  @Post('/policy:policyId/reject')
   async rejectPolicy(@Param('policyId') policyId: number) {
     return this.policyService.rejectPolicy(policyId);
   }
 
-  @Get(':policyId')
+  @Get('/policy:policyId')
   async getPolicy(@Param('policyId') policyId: number) {
     return this.policyService.getPolicy(policyId);
   }
 
-  @Get(':policyId/details')
+  @Get('/policy:policyId/details')
   async getPolicyDetails(@Param('policyId') policyId: number) {
     return this.policyService.getPolicyDetails(policyId);
   }
 
-  @Get()
+  @Get('/policies')
   async listPolicies() {
     return this.policyService.listPolicies();
   }
 
-  @Get('details')
+  @Get('/policies/details')
   async listPolicyDetails() {
     return this.policyService.listPolicyDetails();
   }
