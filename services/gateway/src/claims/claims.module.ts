fc48d5284ea6d9124cd74fb2deddfb297f6b6d93
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2025-05-23 17:24:32 +0300

    feat: integrated claim module

--- services/gateway/src/claims/claims.module.ts
+++ services/gateway/src/claims/claims.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { ClaimsService } from './claims.service';
 import { ClaimsController } from './claims.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [ClaimsController],
   providers: [ClaimsService],
+  imports: [
+        HttpModule,
+        forwardRef(() => AuthModule), 
+      ],
 })
 export class ClaimsModule {}
