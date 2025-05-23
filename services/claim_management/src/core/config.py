b92d9214f3c8ad6d3671e6be4ae8149a19ae5654
Tinsae Tadesse <tinsaetadesse769@gmail.com>
2025-05-23 07:15:50 +0300

    updated core

--- services/claim_management/src/core/config.py
+++ services/claim_management/src/core/config.py
@@ -4,6 +4,7 @@ from pydantic import ConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     POLICY_SERVICE_URL: str
+    CONFIG_SERVICE_URL: str
     API_V1_STR: str = "/api/v1"
     model_config = ConfigDict(from_attributes=True)
 
